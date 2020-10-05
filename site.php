4f13674b6c101fd65be3d54e37513d36fd2119ce
Piramilan <65810943+Piramilan@users.noreply.github.com>
2020-10-05 13:53:08 +0530

    Add My System

---
+++ site.php
@@ -0,0 +1,24 @@
+<?php
+include ("header.html");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body{
+            background-image: url(pt7.jpg);
+        }
+    </style>
+    <title>UOC Library</title>
+</head>
+<body>
+<?php
+include ("index.html");
+?>
+<?php
+include ("footer.html");
+?>
+</body>
+</html>
